fcde8dd469110cb341062f662e6f3cdb35312422
Devilisch <kirowatanabe@gmail.com>
2020-01-12 17:55:28 +0300

    Add functions UpdateEveryRacer and CheckCollides

--- Racers.cs
+++ Racers.cs
@@ -7,41 +7,49 @@ b. Опишите	дальнейшие	изменения и	их	причины
 поведение.	
 */
 
+void UpdateEveryRacer(float deltaTimeS,	List<Racer>	racers) 
+{
+	//	Updates	the	racers	that	are	alive
+	int racerIndex	= 0;
+	for (racerIndex	= 1; racerIndex	<= racers.Count; racerIndex++)
+	{
+		if (racers[racerIndex - 1].IsAlive())
+		{
+			//Racer	update	takes	milliseconds
+			racers[racerIndex - 1].Update(deltaTimeS * 1000.0f);
+		}
+	}
+}
+
+void CheckCollides(List<Racer> racersNeedingRemoved)
+{
+	//	Collides
+	for (int racerIndex1 = 0; racerIndex1 < racers.Count; racerIndex1++)
+	{
+		for (int racerIndex2 = racerIndex1 + 1; racerIndex2 < racers.Count; racerIndex2++)
+		{
+			Racer racer1 = racers[racerIndex1];
+			Racer racer2 = racers[racerIndex2];
+			if (racer1.IsCollidable()	&&	racer2.IsCollidable()	&&	racer1.CollidesWith(racer2))
+			{
+				OnRacerExplodes(racer1);
+                racersNeedingRemoved.Add(racer1);
+                racersNeedingRemoved.Add(racer2);
+			}
+
+		}
+	}
+}
+
 void UpdateRacers(float deltaTimeS,	List<Racer>	racers)
 {
 	List<Racer> racersNeedingRemoved = new List<Racer>();
 	racersNeedingRemoved.Clear();
-	//	Updates	the	racers	that	are	alive
-	int racerIndex	= 0;
-	for (racerIndex	= 1; racerIndex	<= 1000; racerIndex++)
-	{
-		if (racerIndex <= racers.Count)
-		{
-			if (racers[racerIndex - 1].IsAlive())
-			{
-				//Racer	update	takes	milliseconds
-				racers[racerIndex - 1].Update(deltaTimeS * 1000.0f);
-			}
-		}
-	}
-	//	Collides
-	for (int racerIndex1 = 0; racerIndex1 < racers.Count; racerIndex1++)
-	{
-		for (int racerIndex2 = 0; racerIndex2 < racers.Count; racerIndex2++)
-		{
-			Racer racer1 = racers[racerIndex1];
-			Racer racer2 = racers[racerIndex2];
-			if (racerIndex1	!=	racerIndex2)
-			{
-				if (racer1.IsCollidable()	&&	racer2.IsCollidable()	&&	racer1.CollidesWith(racer2))
-				{
-					OnRacerExplodes(racer1);
-                    racersNeedingRemoved.Add(racer1);
-                    racersNeedingRemoved.Add(racer2);
-				}
-			}
-		}
-	}
+
+	UpdateEveryRacer(deltaTimeS,	racers);
+	CheckCollides(racersNeedingRemoved)
+
+	
 	//	Gets	the	racers	that	are	still	alive
 	List<Racer>	newRacerList = new List<Racer>();
 	for (racerIndex	= 0; racerIndex != racers.Count; racerIndex++)
@@ -52,6 +60,7 @@ void UpdateRacers(float deltaTimeS,	List<Racer>	racers)
 	    	newRacerList.Add(racers[racerIndex]);
 		}
 	}
+
 	//	Get	rid	of	all	the	exploded	racers
 	for (racerIndex = 0; racerIndex != racersNeedingRemoved.Count; racerIndex++)
 	{
@@ -62,12 +71,14 @@ void UpdateRacers(float deltaTimeS,	List<Racer>	racers)
 			racers.Remove(racersNeedingRemoved[racerIndex]);
 		}
 	}
+
 	//	Builds	the	list	of	remaining	racers
 	racers.Clear();
 	for (racerIndex	= 0; racerIndex < newRacerList.Count; racerIndex++)
 	{
 		racers.Add(newRacerList[racerIndex]);
 	}
+
 	for (racerIndex = 0; racerIndex < newRacerList.Count; racerIndex++)
 	{
 		newRacerList.RemoveAt(0);
